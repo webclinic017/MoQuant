c1c1ffdf4909d577ef2af50b4b23fe6090f88aa8
yijie.mo <yijie.mo@vipshop.com>
2019-11-21 18:38:50 +0800

    mq calculation - no use file log

--- moquant/log/__init__.py
+++ moquant/log/__init__.py
@@ -12,7 +12,7 @@ level = logging.INFO
 stdout = logging.StreamHandler(sys.stdout)
 file_name = get_env_value('LOG_FILE_NAME')
 file = RotatingFileHandler(file_name if file_name is not None else "mq.log", "w", 10240)
-logging.basicConfig(format=log_formatter, level=level, handlers=[stdout, file])
+logging.basicConfig(format=log_formatter, level=level, handlers=[stdout])
 
 
 def get_logger(name: str):
