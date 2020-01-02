22edb05be5f08bec66c8f1128eb6e8888d6a8d7c
SYSY-Momojie <1069614543@qq.com>
2020-01-02 21:02:18 +0800

    mq - improve sql to clear duplicate

--- moquant/scripts/clear_after_fetch.py
+++ moquant/scripts/clear_after_fetch.py
@@ -5,29 +5,20 @@ log = get_logger(__name__)
 
 
 def clear_duplicate_report_sql(table: str, ts_code: str) -> str:
+    code_where = '1=1' if ts_code is None else 'ts_code = \'%s\'' % ts_code
     table_with_where = table if ts_code is None else '%s where ts_code = \'%s\'' % (table, ts_code)
     return """
-    delete from %s
-    where id in
+    delete a from %s a
+    left join
     (
-        select id
-        from (
-            select 
-                ts.id
-            from
-            (
-                select ts_code, end_date, mq_ann_date, report_type, max(id) as id
-                from %s 
-                group by ts_code, end_date, mq_ann_date, report_type having count(0) > 1
-            ) to_keep
-            left join (select * from %s) ts
-            on to_keep.ts_code = ts.ts_code and to_keep.end_date = ts.end_date 
-            and to_keep.mq_ann_date = ts.mq_ann_date
-            and to_keep.report_type = ts.report_type
-            and to_keep.id != ts.id
-        ) tmp
-    )
-    """ % (table, table_with_where, table_with_where)
+    select ts_code as code, mq_ann_date, end_date, max(id) as id
+    from %s
+    group by ts_code, mq_ann_date, end_date
+    having count(0) > 1
+    ) b
+    on a.ts_code=b.code and a.mq_ann_date=b.mq_ann_date and a.end_date=b.end_date and a.id != b.id
+    where %s and b.code is not null
+    """ % (table, table_with_where, code_where)
 
 
 def clear_duplicate_adjust_report_with(table: str, ts_code: str) -> str:
@@ -48,58 +39,44 @@ def clear_duplicate_adjust_report_with(table: str, ts_code: str) -> str:
 
 
 def clear_duplicate_forecast(table: str, ts_code: str) -> str:
+    code_where = '1=1' if ts_code is None else 'ts_code = \'%s\'' % ts_code
+    table_with_where = table if ts_code is None else '%s where ts_code = \'%s\'' % (table, ts_code)
     return """
-    delete from %s
-    where id in
+    delete ts from %s ts
+    left join
     (
-        select id
-        from (
-            select 
-                ts.id
-            from
-            (
-                select ts_code, end_date, ann_date, max(id) as id
-                from %s 
-                group by ts_code, end_date, ann_date having count(0) > 1
-            ) to_keep
-            left join %s ts
-            on to_keep.ts_code = ts.ts_code and to_keep.end_date = ts.end_date 
-            and to_keep.ann_date = ts.ann_date
-            and to_keep.id != ts.id
-        ) tmp
-    )
-    """ % (table, table if ts_code is None else table + ' where ts_code = \'%s\'' % ts_code, table)
+        select ts_code as code, end_date, ann_date, max(id) as id
+        from %s 
+        group by ts_code, end_date, ann_date having count(0) > 1
+    ) to_keep
+    on to_keep.code = ts.ts_code and to_keep.end_date = ts.end_date 
+    and to_keep.ann_date = ts.ann_date
+    and to_keep.id != ts.id
+    where %s and to_keep.id is not null        
+    """ % (table, table_with_where, code_where)
 
 
 def clear_duplicate_dividend(ts_code):
+    code_where = '1=1' if ts_code is None else 'ts_code = \'%s\'' % ts_code
     return """
-    delete from ts_dividend
-    where id in
+    delete source from ts_dividend source
+    left join
     (
-        select id
-        from
-        (
-            select source.id
-            from
-            (
-                select ts_code, end_date, ann_date, div_proc, max(id) as id
-                from ts_dividend
-                where %s
-                group by ts_code, end_date, ann_date, div_proc
-                having count(0) > 1
-            ) to_keep
-            left join ts_dividend source
-            on to_keep.ts_code=source.ts_code
+        select ts_code as code, end_date, ann_date, div_proc, max(id) as id
+        from ts_dividend where %s
+        group by ts_code, end_date, ann_date, div_proc
+        having count(0) > 1
+    ) to_keep
+    on to_keep.code=source.ts_code
             and to_keep.end_date=source.end_date
             and to_keep.ann_date=source.ann_date
             and to_keep.div_proc=source.div_proc
             and to_keep.id!=source.id
-        ) tmp
-    )
-    """ % ('1=1' if ts_code is None else 'ts_code=\'%s\'' % ts_code)
+    where %s and to_keep.id is not null
+    """ % (code_where, code_where)
 
 
-def clear(ts_code: str):
+def clear(ts_code: str = None):
     db_client.execute_sql(clear_duplicate_report_sql('ts_income', ts_code))
     db_client.execute_sql(clear_duplicate_report_sql('ts_balance_sheet', ts_code))
     db_client.execute_sql(clear_duplicate_adjust_report_with('ts_income', ts_code))
