65c9441df2b2bf9c7004b951bfe123fef5344f90
SYSU-Momojie <1069614543@qq.com>
2021-04-29 21:22:17 +0800

    三缺口买入策略，待测试

--- moquant/simulator/data/price.py
+++ moquant/simulator/data/price.py
@@ -10,21 +10,52 @@ class SimPriceService(object):
     def __init__(self):
         pass
 
-    def get_qfq_price(self, ts_code_arr: list, now_date: str,
-                      from_date: str, to_date: str) -> list:
+    def get_qfq_price_in(self, ts_code_arr: list, now_date: str, date_arr: list):
         """
         获取股票的每日价格信息 - 前复权
         :param ts_code_arr: 股票编码列表
-        :param now_date: 查看数据的日期，会显示当时能看到的前复权价格.
+        :param now_date: 查看数据的日期，会显示当时能看到的前复权价格
+        :param date_arr: 获取价格的时间
+        :return: list(MqDailyPrice)
+        """
+        if ts_code_arr is None or len(ts_code_arr) == 0:
+            date_where = '1=1'
+        else:
+            date_where = "a.trade_date in ('" + "','".join(date_arr) + "')"
+        return self.__common_query(self.__get_code_where(ts_code_arr), now_date, date_where)
+
+    def get_qfq_price(self, ts_code_arr: list, now_date: str, from_date: str, to_date: str) -> list:
+        """
+        获取股票的每日价格信息 - 前复权
+        :param ts_code_arr: 股票编码列表
+        :param now_date: 查看数据的日期，会显示当时能看到的前复权价格
         :param from_date: 获取价格的时间段 开始时间
         :param to_date: 获取价格的时间段 结束时间
-        :return: MqDailyPrice
+        :return: list(MqDailyPrice)
+        """
+        date_where: str = "a.trade_date >= '%s' and a.trade_date <= '%s'" % (from_date, to_date)
+        return self.__common_query(self.__get_code_where(ts_code_arr), now_date, date_where)
+
+    def __get_code_where(self, ts_code_arr: list):
+        """
+        将股票编码列表转化成sql的where
+        :param ts_code_arr:
+        :return:
         """
         if ts_code_arr is None or len(ts_code_arr) == 0:
             code_where = '1=1'
         else:
             code_where = 'a.ts_code in (\'' + '\',\''.join(ts_code_arr) + '\')'
+        return code_where
 
+    def __common_query(self, code_where: str, now_date: str, date_where: str):
+        """
+        封装好的底层查询，按需放入where即可
+        :param code_where: 股票筛选条件
+        :param now_date: 查看数据的日期
+        :param date_where: 查询日期的条件
+        :return:
+        """
         sql = """
             select
                 a.* 
@@ -39,8 +70,6 @@ class SimPriceService(object):
             ) b
             on a.ts_code = b.ts_code and a.div_date = b.div_date
             where a.div_date <= '%s'
-            and a.trade_date >= '%s' and a.trade_date <= '%s'
-            and %s
-            """ % (code_where, now_date, now_date, from_date, to_date, code_where)
-
+            and %s and %s
+            """ % (code_where, now_date, now_date, date_where, code_where)
         return db_client.query_with_sql(sql, MqDailyPrice)
---
+++ moquant/strategy/down_gap_3.py
@@ -0,0 +1,118 @@
+from moquant.constants import mq_daily_metric_enum
+from moquant.dbclient.mq_daily_metric import MqDailyMetric
+from moquant.dbclient.mq_daily_price import MqDailyPrice
+from moquant.log import get_logger
+from moquant.simulator.data import SimDataService
+from moquant.simulator.sim_center import SimCenter
+from moquant.simulator.sim_context import SimContext
+from moquant.simulator.sim_handler import SimHandler
+from moquant.simulator.sim_share_hold import SimShareHold
+from moquant.utils import date_utils
+
+log = get_logger(__name__)
+
+
+class DownGap3(SimHandler):
+
+    def __init__(self):
+        self.target: set = set()
+        self.gap: dict = {}
+        self.last_trade_date: dict = {}  # 上一个交易日
+        self.yesterday_buy: set = set()
+
+    def init(self, context: SimContext, data: SimDataService):
+        # 只对市值超过500亿的做回测
+        mv_arr: list = data.get_daily_metrics(['20210427'], [mq_daily_metric_enum.total_mv.name])
+        for mv in mv_arr:  # type: MqDailyMetric
+            if mv.value >= 500 * 10000 * 10000:
+                self.target.add(mv.ts_code)
+                self.gap[mv.ts_code] = set()
+        pass
+
+    def morning_auction(self, context: SimContext, data: SimDataService):
+        dt: str = context.get_dt()
+        shares: list = context.get_holding()
+        for i in shares:  # type: SimShareHold
+            ts_code: str = i.get_ts_code()
+            p: MqDailyPrice = context.get_today_price(ts_code)
+            gap: set = self.gap[ts_code]
+            if ts_code in self.yesterday_buy:
+                # 第三日无条件卖出
+                context.sell_share(ts_code, i.get_num(), p.down_limit)
+            elif len(gap) < 3:
+                # 已经补了一个缺口，无条件卖出
+                context.sell_share(ts_code, i.get_num(), p.down_limit)
+            else:
+                # 按补缺卖出
+                lp: MqDailyPrice = self.__get_last_trade_day_price(data, ts_code, dt, self.last_trade_date[ts_code])
+                context.sell_share(ts_code, i.get_num(), lp.low_qfq)
+
+    def before_trade(self, context: SimContext, data: SimDataService):
+        dt: str = context.get_dt()
+        to_buy = []
+        for ts_code in self.target:
+            ts_gap: set = self.gap[ts_code]
+            p: MqDailyPrice = context.get_today_price(ts_code)
+
+            if ts_code in self.last_trade_date and p.is_trade == 1:
+                lp: MqDailyPrice = self.__get_last_trade_day_price(data, ts_code, dt, self.last_trade_date[ts_code])
+                if p.open < lp.low_qfq:
+                    # 缺口
+                    ts_gap.add(p.trade_date)
+                    log.info('New gap: %s %s %.2f. Gap num: %d' % (ts_code, context.get_dt(), lp.low_qfq, len(ts_gap)))
+            if len(ts_gap) >= 3:
+                to_buy.append(ts_code)
+
+        # 尽量等权买入
+        for i in range(len(to_buy)):
+            max_num = len(to_buy) - i
+            ts_code = to_buy[i]
+            p: MqDailyPrice = context.get_today_price(ts_code)
+            context.buy_amap(ts_code, p.open, context.get_cash() / max_num)
+
+    def after_trade(self, context: SimContext, data: SimDataService):
+        self.yesterday_buy = set()
+        just_buy: list = context.get_holding_just_buy()
+        for i in just_buy:  # type: SimShareHold
+            self.yesterday_buy.add(i.get_ts_code())
+
+        # 清除已补的缺口，或30天前的缺口
+        dt: str = context.get_dt()
+        old_dt: str = date_utils.period_delta(dt, -30)
+        for ts_code in self.target:
+            p: MqDailyPrice = context.get_today_price(ts_code)
+            if p.is_trade == 0:
+                # 停牌就不用看了
+                continue
+            self.last_trade_date[ts_code] = dt
+            ts_gap: set = self.gap[ts_code]
+
+            if len(ts_gap) == 0:
+                continue
+
+            gap_prices: list = data.get_qfq_price_in([ts_code], dt, list(ts_gap))
+            for gap_price in gap_prices:  # type: MqDailyPrice
+                if gap_price.trade_date < old_dt or p.high >= gap_price.low_qfq:
+                    log.info('Gap clear: %s %s %.2f. Gap num: %d' % (ts_code, dt, gap_price.low_qfq, len(ts_gap)))
+                    ts_gap.remove(p.trade_date)
+
+    def __get_last_trade_day_price(self, data: SimDataService, ts_code: str, now_date: str, trade_date: str):
+        """
+        拿上一个交易日的价格，可能除权了
+        :param data 获取数据服务
+        :param ts_code: 股票编码
+        :param now_date: 目前日期
+        :param trade_date: 交易日期
+        :return:
+        """
+        if ts_code not in self.last_trade_date:
+            return None
+        ltd: str = self.last_trade_date[ts_code]
+        result: list = data.get_qfq_price_in([ts_code], now_date, [trade_date])
+        return result[0] if len(result) > 0 else None
+
+
+if __name__ == '__main__':
+    strategy: DownGap3 = DownGap3()
+    center: SimCenter = SimCenter(strategy, '20210218', '20210427')
+    center.run()
